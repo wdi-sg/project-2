5c65705ab839fe5a6ea27fed8dab2006d7d21841
Keith Chia <email@keithchia.com>
2017-01-22 19:18:28 +0800

    added password hashing and bcrypt comparison function

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
 
 const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 
@@ -18,5 +19,23 @@ const UserSchema = new mongoose.Schema({
   }
 })
 
+UserSchema.methods.checkPassword = function(password) {
+  return bcrypt.compareSync(this.password, password)
+}
+
+UserSchema.options.toJSON = {
+  transform: (doc, ret) => {
+    delete ret.password
+    return ret
+  }
+}
+
+UserSchema.pre('save', function(next) {
+  const hash = bcrypt.hashSync(this.password)
+  this.password = hash
+  next()
+})
+
 const User = mongoose.model('User', UserSchema)
+
 module.exports = User
