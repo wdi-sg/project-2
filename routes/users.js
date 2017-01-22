17dd78619dbef28f357a1a6ebdc0c74d58c71350
Keith Chia <email@keithchia.com>
2017-01-22 18:08:14 +0800

    implemented basic signup

--- routes/users.js
+++ routes/users.js
@@ -1,28 +1,41 @@
+require('colors')
 const express = require('express')
 const User = require('../models/user')
 const router = express.Router()
 
-const ejsValues = {
-  title: 'Sign up'
-}
-
-/* GET users listing. */
 router.get('/', (req, res) => {
   res.redirect('/')
 })
 
 router.get('/signup', (req, res) => {
-  res.render('users/signup', ejsValues)
+  res.render('users/signup', {title: 'Sign up'})
 })
 
 router.get('/login', (req, res) => {
-  res.render('users/login', ejsValues)
+  res.render('users/login', {title: 'Log in'})
+})
+
+router.get('/destroyall', (req, res) => {
+  User.remove({}, (err) => {
+    if (err) console.log(err)
+    else {
+      console.log('ALL USERS HAVE BEEN REMOVED FROM THE DATABASE'.underline.bold.red)
+      res.redirect('/')
+    }
+  })
 })
 
 router.post('/signup', (req, res) => {
   User.create(req.body, (err) => {
-    if (err) console.log(err)
-    else res.redirect('/users/login')
+    if (err) {
+      // console.log(err)
+      console.log('SIGNUP ERROR'.red)
+      res.redirect('/users/signup')
+    }
+    else {
+      console.log('USER SIGNUP SUCCESSFUL'.blue)
+      res.redirect('/users/login')
+    }
   })
 })
 
