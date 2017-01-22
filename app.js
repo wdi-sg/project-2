9fd86545075273c2d8ab7ebde5369645d6d7bb2f
Keith Chia <email@keithchia.com>
2017-01-22 19:57:43 +0800

    added middleware for sessions and login with passport

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const cookieParser = require('cookie-parser')
 const bodyParser = require('body-parser')
 const ejsLayouts = require('express-ejs-layouts')
 const mongoose = require('mongoose')
+const session = require('express-session')
+const passport = require('./config/passport')
+const flash = require('connect-flash')
 
 const index = require('./routes/index')
 const users = require('./routes/users')
@@ -26,7 +29,18 @@ if (process.env.NODE_ENV === 'test') {
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
-// uncomment after placing your favicon in /public
+// login, sessions, flash
+// app.use(flash())
+app.use(session({
+  secret: 'marmalade',
+  // secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+
+// parsing, pathing
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 app.use(logger('dev'))
 app.use(bodyParser.json())
@@ -34,6 +48,13 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(ejsLayouts)
+
+// local variables
+// app.use((res, req, next) => {
+//   // res.locals.flash = req.flash()
+//   res.locals.user = req.user
+//   next()
+// })
 
 app.use('/', index)
 app.use('/users', users)
