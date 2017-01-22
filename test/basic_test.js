ad1d84b83a075df18f1a3ff9de3ee691f7d56cac
Keith Chia <email@keithchia.com>
2017-01-22 17:54:35 +0800

    wrote tests for user signup/login

--- test/basic_test.js
+++ test/basic_test.js
@@ -5,12 +5,17 @@ const app = require('../app')
 require('colors')
 
 describe('BASIC TESTS'.underline, () => {
-  it('should get a 200 response from /', (done) => {
+  it('should get a 200 response from /'.bold, (done) => {
     request(app).get('/')
       .expect(200, done)
   })
-  it('should get redirected from /user to /', (done) => {
+  it('should redirect from /users to /'.bold, (done) => {
     request(app).get('/users')
+      .expect(302)
+      .expect('Location', '/', done)
+  })
+  it('should destroy everything and redirect to / on get to /destroyall'.bold, (done) => {
+    request(app).get('/destroyall')
       .expect(302)
       .expect('Location', '/', done)
   })
---
+++ test/users_test.js
@@ -0,0 +1,81 @@
+/*global describe, it*/
+// const expect = require('chai').expect()
+const app = require('../app')
+const request = require('supertest')
+const server = request.agent(app)
+require('colors')
+
+// DEFINE TEST USERS
+const testUser1 = {
+  email: 'a@b.com',
+  name: 'Boy',
+  password: 'tomato'
+}
+const testUser2 = {
+  email: 'potato',
+  name: 'Boy',
+  passsword: 'potato'
+}
+const testUser3 = {
+  email: 'b@c.com',
+  name: 'Boy',
+  password: 'abc'
+}
+const testUser4 = {
+  email: 'b@c.com',
+  password: 'abc'
+}
+
+// TESTS
+describe('USER LOGIN/SIGNUP AUTH', () => {
+  it('should get a 200 from /users/signup'.bold, (done) => {
+    server.get('/users/signup')
+      .expect(200, done)
+  })
+  it('should get a 200 from /users/login'.bold, (done) => {
+    server.get('/users/login')
+      .expect(200, done)
+  })
+})
+
+describe('USER SIGNUP'.underline, () => {
+  it('should redirect to /users/login on post to /users/signup'.bold, (done) => {
+    server.post('/user/signup')
+      .send(testUser1)
+      .expect('Location', '/users/login', done)
+  })
+  it('should redirect to /users/signup on unsuccessful signup (email already taken)'.bold, (done) => {
+    server.post('/user.signup')
+      .send(testUser1)
+      .expect('Location', '/users/signup', done)
+  })
+  it('should redirect to /users/signup on unsuccessful signup (email format invalid)'.bold, (done) => {
+    server.post('/user.signup')
+      .send(testUser2)
+      .expect('Location', '/users/signup', done)
+  })
+  it('should redirect to /users/signup on unsuccessful signup (password below 5 chars)'.bold, (done) => {
+    server.post('/user.signup')
+      .send(testUser3)
+      .expect('Location', '/users/signup', done)
+  })
+  it('should redirect to /users/signup on unsuccessful signup (no name provided)'.bold, (done) => {
+    server.post('/user.signup')
+      .send(testUser4)
+      .expect('Location', '/users/signup', done)
+  })
+})
+
+describe('USER LOGOUT'.underline, () => {
+  it('should log the user out and redirect to / on get to /users/logout'.bold, (done) => {
+    server.get('/users.logout')
+      .expect('Location', '/', done)
+  })
+})
+
+describe('USER LOGIN'.underline, () => {
+  it('should redirect to /profile on successful login'.bold, (done) => {
+    server.get('/profile')
+      .expect(200, done)
+  })
+})
