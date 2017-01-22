f2a303eeb32d2cdb6315dcadf6b993ad9a286b17
Keith Chia <email@keithchia.com>
2017-01-22 17:55:27 +0800

    added get/post routes for users

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,29 @@
-var express = require('express')
-var router = express.Router()
+const express = require('express')
+const User = require('../models/user')
+const router = express.Router()
+
+const ejsValues = {
+  title: 'Sign up'
+}
 
 /* GET users listing. */
-router.get('/', function(req, res) {
-  // res.send('respond with a resource')
+router.get('/', (req, res) => {
   res.redirect('/')
+})
+
+router.get('/signup', (req, res) => {
+  res.render('users/signup', ejsValues)
+})
+
+router.get('/login', (req, res) => {
+  res.render('users/login', ejsValues)
+})
+
+router.post('/signup', (req, res) => {
+  User.create(req.body, (err) => {
+    if (err) console.log(err)
+    else res.redirect('/users/login')
+  })
 })
 
 module.exports = router
