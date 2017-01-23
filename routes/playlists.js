02bc620613bb72cc468b852b1daa32adba6269bf
Keith Chia <email@keithchia.com>
2017-01-23 13:46:17 +0800

    implemented deleting playlists and tracks via http requests

--- routes/playlists.js
+++ routes/playlists.js
@@ -38,6 +38,44 @@ router.post('/create', (req, res) => {
   })
 })
 
+router.post('/:id/add', (req, res) => {
+  if (logging) console.log('ADDING TRACK TO PLAYLIST: '.blue, req.user.name)
+  const id = req.params.id
+  Playlist.findById(id, (err, doc) => {
+    if (err) return console.log(err)
+    doc.tracks.push(req.body)
+    doc.save((err, updated) => {
+      if (err) return console.log(err)
+      res.send(updated)
+    })
+  })
+})
+
+router.get('/:playlistId/delete/:trackId', (req, res) => {
+  if (logging) console.log('TRACK IS BEING DELETED FROM PLAYLIST: '.blue, req.user.name)
+  const playlistId = req.params.playlistId
+  const trackId = req.params.trackId
+  Playlist.findById(playlistId, (err, doc) => {
+    if (err) return console.log(err)
+    const toRemove = doc.tracks.id(trackId)
+    const removeIndex = doc.tracks.indexOf(toRemove)
+    doc.tracks.splice(removeIndex, 1)
+    doc.save((err, updated) => {
+      if (err) return console.log(err)
+      res.send(updated)
+    })
+  })
+})
+
+router.get('/:id/delete', (req, res) => {
+  if (logging) console.log('PLAYLIST IS BEING DELETED: '.blue, req.user.name)
+  const id = req.params.id
+  Playlist.findByIdAndRemove(id, (err) => {
+    if (err) return console.log(err)
+    res.redirect('/playlists')
+  })
+})
+
 router.get('/:id', (req, res) => {
   if (logging) console.log('SINGLE PLAYLIST ACCESSED BY: '.blue, req.user.name)
   const id = req.params.id
