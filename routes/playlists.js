3c943897b06865574ce1315eacc55cfd60391f06
Keith Chia <email@keithchia.com>
2017-01-23 13:03:07 +0800

    wrote routes for playlists

--- routes/playlists.js
+++ routes/playlists.js
@@ -6,7 +6,18 @@ const router = express.Router()
 const Playlist = require('../models/playlist')
 
 router.get('/', (req, res) => {
-  res.render('playlists', { title: 'Playlists' })
+  // res.render('playlists', { title: 'Playlists' })
+  Playlist.find({})
+    .populate('creator')
+    .populate({
+      path: 'collaborators',
+      model: 'User'
+    })
+    .exec((err, docs) => {
+      if (err) return console.log(err)
+      if (logging) console.log('PLAYLISTS ACCESSED BY: '.blue, req.user.name, ', FOUND'.blue, docs.length, 'PLAYLISTS'.blue)
+      res.send(docs)
+    })
 })
 
 router.get('/destroyall', (req, res) => {
@@ -15,6 +26,31 @@ router.get('/destroyall', (req, res) => {
     if (logging) console.log('ALL PLAYLISTS HAVE BEEN REMOVED FROM THE DATABASE'.bold.underline.red)
     res.redirect('/')
   })
+})
+
+router.post('/create', (req, res) => {
+  if (logging) console.log('PLAYLIST CREATE REQUEST RECEIVED FROM: '.blue, req.user.name)
+  req.body.creator = req.user._id
+  req.body.collaborators.push(req.user._id)
+  Playlist.create(req.body, (err, doc) => {
+    if (err) return console.log(err)
+    res.send(doc)
+  })
+})
+
+router.get('/:id', (req, res) => {
+  if (logging) console.log('SINGLE PLAYLIST ACCESSED BY: '.blue, req.user.name)
+  const id = req.params.id
+  Playlist.findById(id)
+    .populate('creator')
+    .populate({
+      path: 'collaborators',
+      model: 'User'
+    })
+    .exec((err, doc) => {
+      if (err) return console.log(err)
+      res.send(doc)
+    })
 })
 
 module.exports = router
