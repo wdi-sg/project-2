a9c9589ec4cda371c0fc7c188c16ec7093eca126
Keith Chia <email@keithchia.com>
2017-01-23 16:14:42 +0800

    removed excess logging

--- middleware/isLoggedIn.js
+++ middleware/isLoggedIn.js
@@ -7,7 +7,7 @@ module.exports = (req, res, next) => {
     if (logging) console.log('ATTEMPTED TO ACCESS WITHOUT LOGIN'.red)
     res.redirect('/users/login')
   } else {
-    if (logging) console.log('USER ACCESS GRANTED FOR USER: '.blue + req.user.name)
+    // if (logging) console.log('USER ACCESS GRANTED FOR USER: '.blue + req.user.name)
     next()
   }
 }
