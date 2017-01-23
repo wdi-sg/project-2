038cc74032878de57fc6337328f634c14e06238d
Keith Chia <email@keithchia.com>
2017-01-23 16:15:07 +0800

    fixed routes

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,25 @@
+require('colors')
+require('dotenv').config({silent: true})
+let logging = process.env.LOGGING
 const express = require('express')
 const router = express.Router()
+const isLoggedIn = require('../middleware/isLoggedIn')
+const Playlist = require('../models/playlist')
 
 router.get('/', (req, res) => {
   res.render('index', { title: 'My App' })
+})
+
+router.use(isLoggedIn)
+
+router.get('/profile', (req, res) => {
+  if (logging) console.log('PROFILE ACCESSED BY: '.blue, req.user.name)
+  const id = req.user._id
+  Playlist.find({creator: id})
+    .populate('creator')
+    .exec((err, docs) => {
+      res.render('profile', {title: 'Profile', playlists: docs})
+    })
 })
 
 module.exports = router
--- routes/playlists.js
+++ routes/playlists.js
@@ -14,27 +14,34 @@ router.get('/', (req, res) => {
       model: 'User'
     })
     .exec((err, docs) => {
-      if (err) return console.log(err)
+      if (err) return console.log(err.toString().red)
       if (logging) console.log('PLAYLISTS ACCESSED BY: '.blue, req.user.name, ', FOUND'.blue, docs.length, 'PLAYLISTS'.blue)
-      res.send(docs)
+      if (logging) console.log('PLAYLISTS FOUND: '.blue, JSON.stringify(docs, null, 4).blue)
+      res.render('playlists', {title: 'Playlists', playlists: docs})
     })
 })
 
 router.get('/destroyall', (req, res) => {
   Playlist.remove({}, (err) => {
-    if (err) return console.log(err)
+    if (err) return console.log(err.toString().red)
     if (logging) console.log('ALL PLAYLISTS HAVE BEEN REMOVED FROM THE DATABASE'.bold.underline.red)
     res.redirect('/')
   })
 })
 
+router.get('/create', (req, res) => {
+  res.render('playlists/create', {title: 'Create a Playlist'})
+})
+
 router.post('/create', (req, res) => {
   if (logging) console.log('PLAYLIST CREATE REQUEST RECEIVED FROM: '.blue, req.user.name)
   req.body.creator = req.user._id
+  req.body.collaborators = []
   req.body.collaborators.push(req.user._id)
-  Playlist.create(req.body, (err, doc) => {
-    if (err) return console.log(err)
-    res.send(doc)
+  if (logging) console.log(JSON.stringify(req.body, null, 4).blue)
+  Playlist.create(req.body, (err) => {
+    if (err) return console.log(err.toString().red)
+    res.redirect('/profile')
   })
 })
 
@@ -42,10 +49,10 @@ router.post('/:id/add', (req, res) => {
   if (logging) console.log('ADDING TRACK TO PLAYLIST: '.blue, req.user.name)
   const id = req.params.id
   Playlist.findById(id, (err, doc) => {
-    if (err) return console.log(err)
+    if (err) return console.log(err.toString().red)
     doc.tracks.push(req.body)
     doc.save((err, updated) => {
-      if (err) return console.log(err)
+      if (err) return console.log(err.toString().red)
       res.send(updated)
     })
   })
@@ -56,12 +63,12 @@ router.get('/:playlistId/delete/:trackId', (req, res) => {
   const playlistId = req.params.playlistId
   const trackId = req.params.trackId
   Playlist.findById(playlistId, (err, doc) => {
-    if (err) return console.log(err)
+    if (err) return console.log(err.toString().red)
     const toRemove = doc.tracks.id(trackId)
     const removeIndex = doc.tracks.indexOf(toRemove)
     doc.tracks.splice(removeIndex, 1)
     doc.save((err, updated) => {
-      if (err) return console.log(err)
+      if (err) return console.log(err.toString().red)
       res.send(updated)
     })
   })
@@ -71,7 +78,7 @@ router.get('/:id/delete', (req, res) => {
   if (logging) console.log('PLAYLIST IS BEING DELETED: '.blue, req.user.name)
   const id = req.params.id
   Playlist.findByIdAndRemove(id, (err) => {
-    if (err) return console.log(err)
+    if (err) return console.log(err.toString().red)
     res.redirect('/playlists')
   })
 })
@@ -86,7 +93,7 @@ router.get('/:id', (req, res) => {
       model: 'User'
     })
     .exec((err, doc) => {
-      if (err) return console.log(err)
+      if (err) return console.log(err.toString().red)
       res.send(doc)
     })
 })
