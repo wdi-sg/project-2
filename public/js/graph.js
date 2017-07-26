ebb1cf065dec6969b2fa4396598e1b478da45a95
Jocelyn Tan <jocelyntan89@hotmail.com>
2017-07-27 01:15:59 +0800

    added graph

--- public/js/graph.js
+++ public/js/graph.js
@@ -1,32 +1,32 @@
 var ctx = document.getElementById("myChart").getContext('2d')
 var myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
-        datasets: [{
-            label: 'upvotes',
-            data: ['{{country.upvotes}}'],
-            backgroundColor: [
-                'rgba(75, 192, 192, 1)'
-            ],
-            borderWidth: 1
-        }],
-        [{
-            label: 'downvotes',
-            data: ['{{country.downvotes}}'],
-            backgroundColor: [
-                'rgba(255,99,132,1)'
-            ],
-            borderWidth: 1
-        }]
+  type: 'bar',
+  data: {
+    labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+    datasets: [{
+      label: 'upvotes',
+      data: upvotesTotal,
+      backgroundColor: [
+        'rgba(75, 192, 192, 1)'
+      ],
+      borderWidth: 1
     },
+      {
+        label: 'downvotes',
+        data: downvotesTotal,
+        backgroundColor: [
+          'rgba(255,99,132,1)'
+        ],
+        borderWidth: 1
+      }],
     options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero:true
-                }
-            }]
-        }
+      scales: {
+        yAxes: [{
+          ticks: {
+              beginAtZero:true
+          }
+        }]
+      }
     }
+  }
 })
