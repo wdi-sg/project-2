5ca62adb2e852eeb62f9f6a62188959f27e3808e
Jocelyn Tan <jocelyntan89@hotmail.com>
2017-09-19 15:27:07 +0800

    aligned arrows

--- public/js/graph.js
+++ public/js/graph.js
@@ -43,14 +43,149 @@ var myChart = new Chart(ctx, {
     })
   })
 
-  $('#Down1').on('click', function (e) {
+  $('#Up2').on('click', function (e) {
     e.preventDefault()
     $.ajax({
       method: 'POST',
       url: '/votes',
       data: {
-        points: -1,
-        month: 1,
+        points: 1,
+        month: 2,
+        country: $(this).data('country')
+      }
+    }).done(function (data) {
+      location.reload()
+    })
+  })
+
+  $('#Up3').on('click', function (e) {
+    e.preventDefault()
+    $.ajax({
+      method: 'POST',
+      url: '/votes',
+      data: {
+        points: 1,
+        month: 3,
+        country: $(this).data('country')
+      }
+    }).done(function (data) {
+      location.reload()
+    })
+  })
+
+  $('#Up4').on('click', function (e) {
+    e.preventDefault()
+    $.ajax({
+      method: 'POST',
+      url: '/votes',
+      data: {
+        points: 1,
+        month: 4,
+        country: $(this).data('country')
+      }
+    }).done(function (data) {
+      location.reload()
+    })
+  })
+
+  $('#Up5').on('click', function (e) {
+    e.preventDefault()
+    $.ajax({
+      method: 'POST',
+      url: '/votes',
+      data: {
+        points: 1,
+        month: 5,
+        country: $(this).data('country')
+      }
+    }).done(function (data) {
+      location.reload()
+    })
+  })
+
+  $('#Up6').on('click', function (e) {
+    e.preventDefault()
+    $.ajax({
+      method: 'POST',
+      url: '/votes',
+      data: {
+        points: 1,
+        month: 6,
+        country: $(this).data('country')
+      }
+    }).done(function (data) {
+      location.reload()
+    })
+  })
+
+  $('#Up7').on('click', function (e) {
+    e.preventDefault()
+    $.ajax({
+      method: 'POST',
+      url: '/votes',
+      data: {
+        points: 1,
+        month: 7,
+        country: $(this).data('country')
+      }
+    }).done(function (data) {
+      location.reload()
+    })
+  })
+
+  $('#Up8').on('click', function (e) {
+    e.preventDefault()
+    $.ajax({
+      method: 'POST',
+      url: '/votes',
+      data: {
+        points: 1,
+        month: 8,
+        country: $(this).data('country')
+      }
+    }).done(function (data) {
+      location.reload()
+    })
+  })
+
+  $('#Up9').on('click', function (e) {
+    e.preventDefault()
+    $.ajax({
+      method: 'POST',
+      url: '/votes',
+      data: {
+        points: 1,
+        month: 9,
+        country: $(this).data('country')
+      }
+    }).done(function (data) {
+      location.reload()
+    })
+  })
+
+  $('#Up10').on('click', function (e) {
+    e.preventDefault()
+    $.ajax({
+      method: 'POST',
+      url: '/votes',
+      data: {
+        points: 1,
+        month: 10,
+        country: $(this).data('country')
+      }
+    }).done(function (data) {
+      location.reload()
+    })
+  })
+
+  $('#Up11').on('click', function (e) {
+    e.preventDefault()
+    $.ajax({
+      method: 'POST',
+      url: '/votes',
+      data: {
+        points: 1,
+        month: 11,
         country: $(this).data('country')
       }
     }).done(function (data) {
@@ -73,17 +208,182 @@ var myChart = new Chart(ctx, {
     })
   })
 
-  $('#Down12').on('click', function (e) {
-    e.preventDefault()
-    $.ajax({
-      method: 'POST',
-      url: '/votes',
-      data: {
-        points: -1,
-        month: 12,
-        country: $(this).data('country')
-      }
-    }).done(function (data) {
-      location.reload()
-    })
+$('#Down1').on('click', function (e) {
+  e.preventDefault()
+  $.ajax({
+    method: 'POST',
+    url: '/votes',
+    data: {
+      points: -1,
+      month: 1,
+      country: $(this).data('country')
+    }
+  }).done(function (data) {
+    location.reload()
   })
+})
+
+$('#Down2').on('click', function (e) {
+  e.preventDefault()
+  $.ajax({
+    method: 'POST',
+    url: '/votes',
+    data: {
+      points: -1,
+      month: 2,
+      country: $(this).data('country')
+    }
+  }).done(function (data) {
+    location.reload()
+  })
+})
+
+$('#Down3').on('click', function (e) {
+  e.preventDefault()
+  $.ajax({
+    method: 'POST',
+    url: '/votes',
+    data: {
+      points: -1,
+      month: 3,
+      country: $(this).data('country')
+    }
+  }).done(function (data) {
+    location.reload()
+  })
+})
+
+$('#Down4').on('click', function (e) {
+  e.preventDefault()
+  $.ajax({
+    method: 'POST',
+    url: '/votes',
+    data: {
+      points: -1,
+      month: 4,
+      country: $(this).data('country')
+    }
+  }).done(function (data) {
+    location.reload()
+  })
+})
+
+$('#Down5').on('click', function (e) {
+  e.preventDefault()
+  $.ajax({
+    method: 'POST',
+    url: '/votes',
+    data: {
+      points: -1,
+      month: 5,
+      country: $(this).data('country')
+    }
+  }).done(function (data) {
+    location.reload()
+  })
+})
+
+$('#Down6').on('click', function (e) {
+  e.preventDefault()
+  $.ajax({
+    method: 'POST',
+    url: '/votes',
+    data: {
+      points: -1,
+      month: 6,
+      country: $(this).data('country')
+    }
+  }).done(function (data) {
+    location.reload()
+  })
+})
+
+$('#Down7').on('click', function (e) {
+  e.preventDefault()
+  $.ajax({
+    method: 'POST',
+    url: '/votes',
+    data: {
+      points: -1,
+      month: 7,
+      country: $(this).data('country')
+    }
+  }).done(function (data) {
+    location.reload()
+  })
+})
+
+$('#Down8').on('click', function (e) {
+  e.preventDefault()
+  $.ajax({
+    method: 'POST',
+    url: '/votes',
+    data: {
+      points: -1,
+      month: 8,
+      country: $(this).data('country')
+    }
+  }).done(function (data) {
+    location.reload()
+  })
+})
+
+$('#Down9').on('click', function (e) {
+  e.preventDefault()
+  $.ajax({
+    method: 'POST',
+    url: '/votes',
+    data: {
+      points: -1,
+      month: 9,
+      country: $(this).data('country')
+    }
+  }).done(function (data) {
+    location.reload()
+  })
+})
+
+$('#Down10').on('click', function (e) {
+  e.preventDefault()
+  $.ajax({
+    method: 'POST',
+    url: '/votes',
+    data: {
+      points: -1,
+      month: 10,
+      country: $(this).data('country')
+    }
+  }).done(function (data) {
+    location.reload()
+  })
+})
+
+$('#Down11').on('click', function (e) {
+  e.preventDefault()
+  $.ajax({
+    method: 'POST',
+    url: '/votes',
+    data: {
+      points: -1,
+      month: 11,
+      country: $(this).data('country')
+    }
+  }).done(function (data) {
+    location.reload()
+  })
+})
+
+$('#Down12').on('click', function (e) {
+  e.preventDefault()
+  $.ajax({
+    method: 'POST',
+    url: '/votes',
+    data: {
+      points: -1,
+      month: 12,
+      country: $(this).data('country')
+    }
+  }).done(function (data) {
+    location.reload()
+  })
+})
