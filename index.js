67fdfddd0b9eb958fb841bda5318adf1db767ac8
koozy <koozhengyu@gmail.com>
2017-10-23 17:55:07 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,61 @@
+// setup environment
+require('dotenv').config({ silent: true })
+
+// setting dburl and port
+const dbUrl = process.env.MONGODB_URI|| 'mongodb://localhost/project2'
+const port = process.env.PORT || 4000
+
+// installing all modules
+const express = require('express') // express
+const path = require('path') // for Public files
+const mongoose = require('mongoose') // mongoose
+const exphbs  = require('express-handlebars') // handlebars
+const bodyParser = require('body-parser') // for accessing POST request
+const session = require('express-session')
+const MongoStore = require('connect-mongo')(session)
+const passport = require('./config/ppConfig')
+
+// initiating express
+const app = express()
+
+// view engine setup
+app.engine('handlebars', exphbs({defaultLayout: 'main'}))
+app.set('view engine', 'handlebars')
+
+// static path setup
+app.use(express.static(path.join(__dirname, 'public')))
+
+// setup bodyParser
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
+
+// connect to mongodb via mongoose
+mongoose.Promise = global.Promise
+mongoose.connect(dbUrl, {
+  useMongoClient: true
+})
+.then(
+  () => { console.log('Connected to database') },
+  (err) => { console.log(err) }
+)
+
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true,
+  // store this to our db too
+  store: new MongoStore({ mongooseConnection: mongoose.connection })
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+app.get('/', (req, res) => {
+  res.render('home')
+})
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`)
+})
---
+++ models/user.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const bcrypt = require('bcrypt')
+
+const userSchema = new Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  slug: String // new field for vanity url
+})
+
+userSchema.pre('save', function(next) {
+  var user = this
+  user.slug = user.name.toLowerCase().split(' ').join('-')
+
+  bcrypt.hash(user.password, 10)
+  .then(hash => {
+    user.password = hash
+    console.log('pre save flow', user)
+    next() // next() is calling the save()
+  })
+})
+
+userSchema.methods.validPassword = function (plainPassword, callback) {
+  bcrypt.compare(plainPassword, this.password, callback)
+}
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = User
