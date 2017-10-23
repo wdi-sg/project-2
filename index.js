77410f5831be95f95d6eb71ad07a39a33289517e
noobnur <nurhsans@gmail.com>
2017-10-23 17:25:37 +0800

    first commit - set up homepage

---
+++ index.js
@@ -0,0 +1,22 @@
+require('dotenv').config({silent: true})
+
+// ======= Setup of Dependencies ======= //
+const express = require('express')
+const exphbs = require('express-handlebars')
+
+const port = 3000
+const app = express()
+
+// ======= Setup of main GET reqs ======= //
+/* Using handlebars template engine */
+app.engine('handlebars', exphbs({defaultLayout: 'main'}))
+app.set('view engine', 'handlebars')
+
+app.get('/', (req, res) => {
+  res.render('home')
+})
+
+// ======= END: Local port Listen ======= //
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`)
+})
