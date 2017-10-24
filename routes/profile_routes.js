6b140de4fbd4a95cbb15e0ad2cf914ffb2debb67
noobnur <nurhsans@gmail.com>
2017-10-24 20:19:27 +0800

    profile card ok

--- routes/profile_routes.js
+++ routes/profile_routes.js
@@ -25,33 +25,24 @@ router.get('/:slug/settings', (req, res) => {
   })
 })
 
-router.get('/profile/:slug/card', (req, res) => {
-  res.render('users/card')
-})
-
 router.post('/:slug/settings', (req, res) => {
   var profileData = req.body
   User.findOne({
     slug: req.params.slug
   })
-// this part a bit funny. Need to fix this.
   .then((user) => {
     user.profile.push({
       nickname: profileData.nickname,
-      about: profileData.about
-      // skills: profileData.skills,
-      // projs: profileData.projs,
-      // contact: profileData.contact
+      about: profileData.about,
+      skills: profileData.skills,
+      projs: profileData.projs,
+      contact: profileData.contact
     })
     user.save()
     .then(
-      user => res.redirect(`/profile/${user.slug}/card`),
+      user => res.redirect(`/profile/${user.slug}`),
       err => res.send(err)
     )
-    // .save(function (err) {
-    //   if (!err) console.log('Success!')
-    // })
-    // res.redirect('users/card', user)
   })
 })
 
