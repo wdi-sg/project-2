9d2be2890bc8bbdef8b947f5cb01509359f8d97f
subutai1175 <subutai1175@gmail.com>
2017-10-24 11:59:48 +0800

    added handlebar layouts home main pages

--- index.js
+++ index.js
@@ -1,26 +1,38 @@
-const express = require('express')
-const mongoose = require('mongoose');
+const express = require("express")
+const mongoose = require("mongoose")
+const exphbs = require("express-handlebars")
+const bodyParser = require("body-parser")
 const app = express()
 
 
-const dbUrl = process.env.MONGODB_URI === 'production' ? process.env.MONGODB_URI : "mongodb://localhost/project2"
+const dbUrl = process.env.MONGODB_URI === "production" ? process.env.MONGODB_URI : "mongodb://localhost/project2"
 const port = process.env.PORT || 3000
 
-app.get('/', (req, res) => {
-  res.send("Connected")
+// require userSchema
+const User = require("./models/user")
+
+// View engine handlebars setup
+app.engine("handlebars", exphbs({ defaultLayout: "main"}))
+app.set("view engine", "handlebars")
+
+
+app.get("/", (req, res) => {
+	res.render("home")
 })
 
 // connectin to mongodb
 mongoose.Promise = global.Promise
 
 mongoose.connect(dbUrl, {
-  useMongoClient: true
+	useMongoClient: true
 })
-.then(
-  () => { console.log('db is connected :P') },
-  (err) => { console.log(err) }
-)
+	.then(
+		() => { console.log("db is connected :P") },
+		(err) => { console.log(err) }
+	)
 
+
+// set port for express to listen to
 app.listen(port, () => {
-  console.log(`Server is running on ${port}`);
+	console.log(`Server is running on ${port}`)
 })
