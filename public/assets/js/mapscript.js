47383487639039ef51782a9546bb60a8f04c6b1b
Gabriel <gabrielulu@gmail.com>
2017-10-24 18:44:12 +0800

    autocomplete v1 working

--- public/assets/js/mapscript.js
+++ public/assets/js/mapscript.js
@@ -5,9 +5,10 @@ function initMap () {
     center: {lat: 1.307820, lng: 103.831830},
     zoom: 19
   })
-  // var input = $('#field1')
-  // var autocomplete = new google.maps.places.Autocomplete(input)
-  // autocomplete.bindTo('bounds', map)
+  var input = $('#field1')
+  console.log(input[0])
+  var autocomplete = new google.maps.places.Autocomplete(input[0])
+  autocomplete.bindTo('bounds', map)
 
   directionsDisplay.setMap(map)
 
@@ -16,7 +17,7 @@ function initMap () {
   }
   $('#start').on('change', onChangeHandler)
   $('#end').on('change', onChangeHandler)
-        }
+}
 function calculateAndDisplayRoute (directionsService, directionsDisplay) {
   directionsService.route({
     origin: $('#start').val(),
