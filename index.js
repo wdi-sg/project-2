24abd6ff730dc6fba4264a8357c40090c398bcc3
subutai1175 <subutai1175@gmail.com>
2017-10-24 08:28:13 +0800

    set up mongoose connect

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 const express = require('express')
-
+const mongoose = require('mongoose');
 const app = express()
 
-const port = 3000
+
+const dbUrl = process.env.MONGODB_URI === 'production' ? process.env.MONGODB_URI : "mongodb://localhost/project2"
+const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
   res.send("Connected")
 })
+
+// connectin to mongodb
+mongoose.Promise = global.Promise
+
+mongoose.connect(dbUrl, {
+  useMongoClient: true
+})
+.then(
+  () => { console.log('db is connected :P') },
+  (err) => { console.log(err) }
+)
 
 app.listen(port, () => {
   console.log(`Server is running on ${port}`);
