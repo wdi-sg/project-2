787d333c58417cef85b5d6195bc6068e4b202bfa
noobnur <nurhsans@gmail.com>
2017-10-25 22:57:35 +0800

    fixed pre'save. now doesnt change the password after saving new profile

--- models/user.js
+++ models/user.js
@@ -21,6 +21,9 @@ const userSchema = new Schema({
 })
 
 userSchema.pre('save', function (next) {
+  if (!this.isModified('password')) {
+    return next()
+  }
   var user = this
   user.slug = user.name.toLowerCase().split(' ').join('-')
   bcrypt.hash(user.password, 10)
