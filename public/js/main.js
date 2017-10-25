040df8a1ffa35e3f694a295ad9ac5a7a27e912d7
sm <a0086845@u.nus.edu>
2017-10-25 14:27:48 +0800

    completed chat

--- public/js/main.js
+++ public/js/main.js
@@ -1,14 +1,15 @@
 const $message = $("#message")
+const $userName = $("#userName").val()
 
 $(function() {
   let socket = io()
   $message.submit(function() {
-    console.log("message form pressed")
+    // console.log("message form pressed")
     socket.emit("chat message", $("#m").val())
     $("#m").val("")
     return false
   })
   socket.on("chat message", function(msg) {
-    $("#messages").append($("<li>").text(msg))
+    $("#messages").append($("<li>").text($userName + ": " + msg))
   })
 })
