a08263d64426afac95c22ef1aeffcabc17ad0cc6
hello <31865878+mingyikoh@users.noreply.github.com>
2017-10-27 18:39:24 +0800

    User update and delete

--- routes/profile_routes.js
+++ routes/profile_routes.js
@@ -7,4 +7,21 @@ router.get('/', (req, res) => {
   res.render('users/profile')
 })
 
+router.put('/', (req, res) => {
+  var formData = req.body
+  User.findByIdAndUpdate(req.user.id, {
+    name: formData.username,
+    email: formData.email
+  })
+  .then(() => {
+    res.redirect('/profile')
+  })
+})
+
+router.delete('/', (req, res) => {
+  User.findByIdAndRemove(req.user.id)
+  .then(() => res.redirect(`/`))
+  .catch(err => console.log(err))
+})
+
 module.exports = router
