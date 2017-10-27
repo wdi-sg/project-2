d8371ac68f03a593ebaacd26ef1b63927c8323c2
noobnur <nurhsans@gmail.com>
2017-10-27 21:20:11 +0800

    fixed gigs-search. All major functions working

--- public/js/frontend.js
+++ public/js/frontend.js
@@ -88,36 +88,38 @@ $(function () {
 
 /* Search Gigs Section */
 
-  $searchGigs.on('keyup', e => { // e is the event object of the keyup event
-    var keyword = e.target.value
-
+  $searchGigs.on('keyup', g => { // e is the event object of the keyup event
+    $('#allGigCards').remove()
+    var keyword = g.target.value
     var json = JSON.stringify({
       keyword
     })
 
-    fetch('/gigs', {
+    fetch('/', {
       method: 'POST',
       body: json,
       headers: {
         'Content-Type': 'application/json'
       }
     })
-  .then(response => response.json()) // convert the json file into js object
+  .then(response => response.json())
+  // convert the json file into js object
   .then(showGigs) // at this point we got the data
   .catch(err => console.log(err))
   })
 
   function showGigs (data) {
-    let allGigs = data.map(user => {
+    console.log(data)
+    let allGigs = data.map(gig => {
       const $newCol = $('<div class="col-4">')
       const $newCard = $('<div class="card">')
       const $newCardBody = $('<div class="card-body">')
       const $newCardTitle = $('<h4 class="card-title">')
       const $newCardText = $('<p class="card-text">')
 
-      $newCardTitle.text(gigs.name)
+      $newCardTitle.text(gig.name)
       $newCardText.html(
-      `${gigs.wage} ${gigs.author}
+      `${gig.wage} ${gig.author}
       `
     )
 
@@ -144,4 +146,5 @@ $(function () {
     var newValue = prev ? `${prev}, ${str}` : `${str}`
     $('#skills').attr('value', newValue)
   })
+
 })
