ae2cb049ee0ddcea9d5cfff16b9570431bea15b6
koozy <koozhengyu@gmail.com>
2017-10-27 13:38:56 +0800

    add favs functionality, to fix bug allowing multiple favs by same user

--- routes/profile_routes.js
+++ routes/profile_routes.js
@@ -13,4 +13,29 @@ router.post('/', (req, res) => {
   res.redirect(`/profile/${username}`)
 })
 
+router.put('/:username/follow', (req, res) => {
+  var userId = req.body.id // user to follow
+  var currUserId = req.user.id // current user
+  console.log('userId: ', userId)
+  console.log('currUserId: ', currUserId)
+  console.log('currUserName: ', req.user.name)
+  User.findById(userId)
+  .then(user => {
+    if(user.id !== currUserId) {
+      console.log("found user's username: ", user.username)
+      console.log('not current user')
+
+      // check if currUserId already exists in followers array
+      // if not, push
+
+      user.following.push(currUserId)
+      user.save()
+      .then(() => {
+        console.log('saved!')
+        res.redirect('/')
+      })
+    }
+  })
+})
+
 module.exports = router
