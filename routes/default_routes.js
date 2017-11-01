c3d1f16fead83523f6ea1d0bae6febb2ef1b70a2
josephpung <goldenpummel@live.com>
2017-11-01 22:13:43 +0800

    CHANGED TO DATE SORT

--- routes/default_routes.js
+++ routes/default_routes.js
@@ -12,7 +12,7 @@ const { hasLoggedOut, isLoggedIn } = require('../helpers')
 router.get('/', (req, res) => {
   if(req.user === undefined || req.user.coursePref === "no"){
 
-    Thread.find().sort({totalVotes: 1})
+    Thread.find().sort({sortDate: -1}) // CHANGED TO DATE SORT, READ HERE PLS
     .then(result=>{
       res.render("user/home",{
         title: "Questions",
@@ -30,7 +30,7 @@ router.get('/', (req, res) => {
         title: "Questions",
         threads: data
       })
-    }).sort({totalVotes: 1})
+    }).sort({totalVotes: -1})
   }
 
 })
