a3870ccaf6ece4db95bb9b5736a6d0036b1e2fb3
Darren Koh <darren212@gmail.com>
2018-01-09 00:56:56 +0800

    Nav bar update

--- helpers/loginBlock.js
+++ helpers/loginBlock.js
@@ -1,4 +1,5 @@
 module.exports = (req,res,next) => {
+  console.log(req.user)
   if (!req.user) {
     req.flash('error', 'You must be logged in to access that page')
     res.redirect('/auth/login')
