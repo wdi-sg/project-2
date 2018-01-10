97ab023db6c9b48c13195231ab68aca01e184261
Darren Koh <darren212@gmail.com>
2018-01-10 11:18:02 +0800

    Fixed new trip issue

--- controllers/tripController.js
+++ controllers/tripController.js
@@ -28,45 +28,41 @@ exports.create = (req,res) => {
         req.flash('error', 'Trip not created')
         res.redirect('/trip/new')
       } else {
-        req.session.currentTripID = trip._id
         req.flash('success', 'Trip created')
-        res.redirect('/trip/main')
+        res.redirect('/trip/main?id='+trip.id)
       }
     }) //end err
   } //end else
 }
 
 exports.main = (req,res) => {
-  let currentTripID = ""
-  if (req.session.currentTripID != null) {
-    currentTripID = req.session.currentTripID
-    req.session.currentTripID = null
-  }
-  else if (req.query.id != null) {
-    currentTripID = req.query.id
-  }
-  let query = {
-    "_id" : currentTripID
-  }
-
-  Trip.findOne(query).exec((err, data) => {
-    if (err) {
-      console.log(err)
-      req.flash('error', 'Trip not found')
-      res.redirect('/home')
+  if (req.query.id != null) {
+    let query = {
+      "_id" : req.query.id
     }
-    else {
-      Location.find({
-        tripId: data.id
-      }).exec((err2, data2) => {
-        if (err2) {
-          console.log(err2)
-        }
-        else {
-          res.render('trip/tripMain',{"results":data, "results2":data2})
-          // res.render('trip/tripMain',{"results":data})
-        }
-      })
-    }
-  })
+    Trip.findOne(query).exec((err, data) => {
+      if (err) {
+        console.log(err)
+        req.flash('error', 'Trip not found')
+        res.redirect('/home')
+      }
+      else {
+        Location.find({
+          tripId: data.id
+        }).exec((err2, data2) => {
+          if (err2) {
+            console.log(err2)
+          }
+          else {
+            res.render('trip/tripMain',{"results":data, "results2":data2})
+            // res.render('trip/tripMain',{"results":data})
+          }
+        })
+      }
+    })
+  }//End if query id not null
+  else {
+    req.flash('error', 'Trip not found')
+    res.redirect('/home')
+  }
 }
