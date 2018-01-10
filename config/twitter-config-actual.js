f4217807481b9e28107ec2f33b647473b0450716
Lim Jiechao <jiechao.lim@gmail.com>
2018-01-10 12:01:12 +0800

    preparation for heroku

---
+++ config/twitter-config-actual.js
@@ -0,0 +1,5 @@
+module.exports = {
+	consumerKey: 'qBOOC1ZKNqEKlRlwIMMei5wLD',
+	consumerSecret: 'zy3nBFPHmnRyNLY9Q8crW1AyGtCJfijByJPzmWehuQLP7iQYQQ',
+	callbackURL: 'http://127.0.0.1:3000/twitter/callback'
+}
