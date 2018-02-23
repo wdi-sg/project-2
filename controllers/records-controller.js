18fb8a0d76a8011cadb3985e9797f86488cea5eb
Lim Jiechao <jiechao.lim@gmail.com>
2018-02-23 23:51:31 +0800

    moved API maps into separate directory
    Signed-off-by: Lim Jiechao <jiechao.lim@gmail.com>

--- controllers/records-controller.js
+++ controllers/records-controller.js
@@ -1,7 +1,7 @@
 const Divination = require('../models/divination');
-const directory = require('../models/directory');
-const api = require('../models/yijing-yilin');
-const parallel = require('../models/parallel-text');
+const directory = require('../data/directory');
+const api = require('../data/yijing-yilin');
+const parallel = require('../data/parallel-text');
 const reference = {
 	url: {
 		api: 'https://api.ctext.org/gettext?urn=ctp:',
