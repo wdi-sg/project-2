c4fcc9cd571cd4febaaa0b5ee966c984b4597d54
Lim Jiechao <jiechao.lim@gmail.com>
2018-03-08 09:44:52 +0800

    actual db and Twitter credentials
    Signed-off-by: Lim Jiechao <jiechao.lim@gmail.com>

---
+++ config/config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    mongodb: {
+        "url" : "mongodb://H7af9xJosr8CE7b8o97C8LTF48ngptkg:K3VXqgCFQoms4cw8X9xpTqJ9co23yytTtqWDww4XyGqLhszsY2DWVxAy4wTGcqoq@ds133557.mlab.com:33557/oracle"
+        // "url" : "mongodb://localhost/oracle"
+    },
+    session: {
+        secret: 'cannotfindmodulebabeleslint'
+    },
+    twitter: {
+        consumerKey: 'kJ45WcrPdlUdpbZOi6hYKS83K',
+        consumerSecret: '1l66C0SCi4moeQKX6Uyn7QlRNsjPbbplNg9go1L8tavesfMFzl',
+        callbackURL: 'https://oracleofchanges.herokuapp.com/twitter/callback'
+        // callbackURL: 'http://127.0.0.1:3000/twitter/callback'
+    }
+}
